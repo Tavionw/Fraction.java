460381c43af658c2bb601dc656a5abef0d67fe5e
Tavionw <97750928+Tavionw@users.noreply.github.com>
2022-01-29 18:18:05 -0500

    Create Fraction.java

---
+++ Fraction.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Fraction
+{
+        public static void main(String[] args)
+        {
+    // declare a scanner object for input
+                Scanner scan = new 
+
+    // prompt for numerator and read in - use print instead of println so the user will input on the same line as the prompt
+    int num = 3;
+    // prompt for denomator and read in - use print instead of println so the user will input on the same line as the prompt
+    int dem = 6;
+    // compute the answer and store in a variable of type double
+    System.out.print((double) numerator/denomator);
+    // ouutput the answer
+    
+  }
+}
